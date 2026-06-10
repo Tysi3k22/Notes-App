e56f6a51f9864a7c77006ca6aa9a9e5dd264071a
Tysi3k22 <kacpert.kasiak@gmail.com>
2026-06-10 02:18:10 +0200

    dodanie funkcjonalonsci dodawania notatek do localstorage

--- script.js
+++ script.js
@@ -11,39 +11,59 @@ let options = {
 
 let formatedDate = new Intl.DateTimeFormat("pl", options).format(date);
 
-const notesArr = [];
+const notesArr = JSON.parse(localStorage.getItem('notesArr')) || [];
+
+function saveItem(item) {
+    notesArr.push(item);
+    saveToLocalStorage();
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('notesArr' ,JSON.stringify(notesArr));
+}
 
 function removeItem(item, element) {
-    let index = notesArr.findIndex(item => item === item.text)
+    const index = notesArr.findIndex(note => note.text === item.text);
 
-    if(index !== -1) {
-        notesArr.slice(index, 1);
+    if (index !== -1) {
+        notesArr.splice(index, 1);
+        saveToLocalStorage();
     }
 
     element.remove();
 }
 
 addNoteBtn.addEventListener("click", () => {
+    let noteTitle = document.getElementById('Note').value;
+
+    createItem(noteTitle, "", formatedDate);
+})
+
+function loadItems() {
+    notesArr.forEach(el => {
+        createItem(el.text, el.dis, el.date, false);
+    });
+}
+
+function createItem(text, dis, date, save = true) {
     let div = document.createElement('div');
     let removeItemBtn = document.createElement('button')
     let header1 = document.createElement('h2');
     let header2 = document.createElement('h5');
 
-    let noteTitle = document.getElementById('Note').value;
-
-    if(noteTitle === "") return;
+    if(text === '') return;
     
     div.classList.add('notesItem');
     removeItemBtn.classList.add('removeItem')
 
-    header1.innerHTML = noteTitle;
-    header2.innerHTML = formatedDate;
+    header1.innerHTML = text;
+    header2.innerHTML = date;
     removeItemBtn.innerHTML = '🗑️';
 
     let item = {
-        text: noteTitle,
-        dis: '',
-        date: formatedDate
+        text: text,
+        dis: dis, // miejsce na potecjalny tekst
+        date: date
     }
 
     removeItemBtn.addEventListener("click", () => {
@@ -51,11 +71,12 @@ addNoteBtn.addEventListener("click", () => {
     })
 
 
-
-    notesArr.push(item);
-
+    if(save) saveItem(item);
+    
     div.appendChild(header1)
     div.appendChild(removeItemBtn)
     div.appendChild(header2)
     notes.appendChild(div);
-})
+}
+
+loadItems();
